b632f1fe82e6324fd4aa903e663b3c467cd9ffa5
johncongercc <johnconger@gmail.com>
2019-11-26 20:00:45 -0500

    5

--- Program.cs
+++ Program.cs
@@ -34,6 +34,14 @@ namespace crypto
             var ExpectedIndex = 170;
             Console.WriteLine(IndexOfRecInFile == ExpectedIndex);
 
+            //https://cryptopals.com/sets/1/challenges/5
+            string PlainText1 = "Burning 'em, if you ain't quick and nimble";
+            string PlainText2 = "I go crazy when I hear a cymbal";
+            string key = "ICE";
+            string CipherText = RepeatingOR(PlainText1+'\n'+PlainText2, key);
+            string ExpectedCipher = "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f";
+            Console.WriteLine(CipherText == ExpectedCipher);
+            
         }
 
         static int DetectSingleCharacterXOR()
@@ -90,26 +98,6 @@ namespace crypto
             }
             return score;
         }
-        static string ConvertHexStringToAscii(String hexString)
-        {
-            try
-            {
-                string ascii = string.Empty;
-
-                for (int i = 0; i < hexString.Length; i += 2)
-                {
-                    String hs = string.Empty;
-
-                    hs   = hexString.Substring(i,2);
-                    uint decval =   System.Convert.ToUInt32(hs, 16);
-                    char character = System.Convert.ToChar(decval);
-                    ascii += character;
-                }
-                return ascii;
-            }
-            catch (Exception ex) { Console.WriteLine(ex.Message); }
-            return string.Empty;
-        }
 
         static string FillFullHex(int length, string Hex)
         {
@@ -119,6 +107,30 @@ namespace crypto
                 SB.Append(Hex);
             }
             return SB.ToString();
+        }
+        static string RepeatingOR(string PlainText, string key)
+        {
+            var Hex1 = ConvertStringToHexAscii(PlainText);
+            var Hex2 = PadKeyToSize(ConvertStringToHexAscii(key), Hex1.Length);
+            return FixedOR(Hex1, Hex2);
+        }
+
+        static string PadKeyToSize(string HexKey, int size)
+        {
+            var SB = new StringBuilder();
+            for (int i = 0; i < size/2; i++)
+            {
+                string Hex = GetNextHex(HexKey, i);
+                SB.Append(Hex);
+            }
+            return SB.ToString();
+        }
+
+        static string GetNextHex(string HexKey, int i)
+        {
+            int HexSize = HexKey.Length / 2;
+            int Index = i % HexSize;
+            return HexKey.Substring(Index * 2, 2);
         }
         static string FixedOR(string hex1, string hex2)
         {
@@ -184,6 +196,35 @@ namespace crypto
             }
 
             return result.ToString().ToLower();
+        }
+
+        static string ConvertHexStringToAscii(String hexString)
+        {
+            try
+            {
+                string ascii = string.Empty;
+
+                for (int i = 0; i < hexString.Length; i += 2)
+                {
+                    String hs = string.Empty;
+
+                    hs   = hexString.Substring(i,2);
+                    uint decval =   System.Convert.ToUInt32(hs, 16);
+                    char character = System.Convert.ToChar(decval);
+                    ascii += character;
+                }
+                return ascii;
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+            return string.Empty;
+        }
+
+        public static string ConvertStringToHexAscii(string input)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach(char c in input)
+                sb.AppendFormat("{0:X2}", (int)c);
+            return sb.ToString().Trim();
         }
 
         static Dictionary<char, double> GetEngCharFreq()
